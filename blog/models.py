7b7409bc3185eaa305a27b46974dc6f2760d5b19
ilnurgi <ilnurgi87@gmail.com>
2016-04-26 15:23:38 +0300

    new highlight

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,16 @@ import re
 
 from django.contrib import admin
 from django.db import models
+
 from markdown import markdown
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
 
 from comments.models import Comment
 
 re_references = re.compile(ur'\[(.+?)\]\[(.+?)\]')
-re_pre_code = re.compile(ur'<pre><code>(.+?)</code></pre>')
-re_unicode = re.compile(ur'(u\'(.*?)\')')
 
 
 class Tags(models.Model):
@@ -65,74 +68,37 @@ class Post(models.Model):
         return content
 
     def add_code_style(self, content):
-        is_code = False
         code_start = u'<pre><code>'
         code_end = u'</code></pre>'
         code_start_len = len(code_start)
-        code_end_len = len(code_end)
-        _content = []
 
-        for line in content.split('\n'):
+        is_code = False
+        _lexer = 'text'
+        code = []
+        result = []
+
+        for line in content.splitlines():
             if line.startswith(code_start):
                 is_code = True
-                _line = line[code_start_len:]
+                _lexer = line[code_start_len:].strip()
             elif line.endswith(code_end):
                 is_code = False
-                _line = line[:code_end_len]
+                lexer = get_lexer_by_name(_lexer, stripall=True)
+                formatter = HtmlFormatter()
+                result.append(
+                    highlight(
+                        u'\n'.join(
+                            i.replace('&lt;', '<').replace('&gt;', '>')
+                            for i in code),
+                        lexer,
+                        formatter))
+                code = []
+            elif is_code:
+                code.append(line)
             else:
-                _line = line
+                result.append(line)
 
-            _new_line = _line
-            if is_code:
-                if _new_line.strip().startswith(u'#'):
-                    _new_line = u'<span class="comment">{0}</span>'.format(
-                        _new_line)
-                else:
-                    for kw in (u'import ', u'try:', u'except ', u'print ',
-                               u'if ', u'is not ', u'not ', u'for ', u'in '):
-                        if _new_line.startswith(kw):
-                            _new_line = _new_line.replace(
-                                kw.strip(),
-                                u'<span class="keyword">{0}</span>'.format(
-                                    kw.strip()),
-                                1)
-                        else:
-                            _new_line = _new_line.replace(
-                                u' {0}'.format(kw),
-                                u' <span class="keyword">{0}</span> '.format(
-                                    kw.strip()))
-
-                    for kw in (u'True', u'False'):
-                        if _new_line.startswith(u'{0} '.format(kw)):
-                            _new_line = _new_line.replace(
-                                kw,
-                                u'<span class="bool">{0}</span>'.format(
-                                    kw.strip()),
-                                1)
-                        else:
-                            _new_line = _new_line.replace(
-                                u' {0} '.format(kw),
-                                u' <span class="bool">{0}</span> '.format(
-                                    kw.strip())).replace(
-                                u' {0}:'.format(kw),
-                                u' <span class="bool">{0}:</span>'.format(
-                                    kw.strip()))
-
-                    for exc in (u'IndexError', ):
-                        _new_line = _new_line.replace(
-                            u' {0}:'.format(exc),
-                            u'<span class="exception"> {0}:</span>'.format(exc))
-
-                    for items in re_unicode.findall(_new_line):
-                        _new_line = _new_line.replace(
-                            items[0],
-                            u'<span class="unicode">u</span>'
-                            u'<span class="string">\'{0}\'</span>'.format(
-                                items[1]))
-
-            _content.append(line.replace(_line, _new_line))
-
-        return u'\n'.join(_content)
+        return u'\n'.join(result)
 
     def tag_names(self):
         return u', '.join(
